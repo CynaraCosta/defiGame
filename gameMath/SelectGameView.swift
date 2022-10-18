3c5d88945e5e4b25616991400d6956810b9b101f
CynaraCosta <cvocac@cin.ufpe.br>
2022-10-18 08:51:15 -0300

    carosel not working dynamic

--- gameMath/SelectGameView.swift
+++ gameMath/SelectGameView.swift
@@ -7,9 +7,30 @@
 
 import SwiftUI
 
+struct DestinationView: View {
+    @Binding var isPresenting: Bool
+    
+    var game: String
+    
+    var body: some View {
+        
+        Text("Jogo \(game)")
+        
+        //        .sheet(isPresented: $isPresenting, content: {
+        //            SelectGameView()
+        //    })
+    }
+}
+
+
 struct SelectGameView: View {
     
     @State private var index = 0
+    
+    @ObservedObject var gamesViewModel: GamesViewModel = .init()
+    
+    @State var isItemViewPresenting: Bool = false
+    //    @StateObject var selectedItem: Game
     
     var body: some View {
         ZStack {
@@ -17,29 +38,44 @@ struct SelectGameView: View {
             
             VStack {
                 
-//                TabView(selection: $index) {
-//                    ForEach(0..<4) { index in
-//                        CardView()
-//                    }
-//                }.tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
-                
-                
                 ScrollView(.horizontal, showsIndicators: false) {
                     HStack(alignment: .top, spacing: 16) {
-                        ForEach(games) { i in
+                        ForEach(gamesViewModel.games) { game in
                             
+                            NavigationLink(isActive:  $isItemViewPresenting,
+                                           destination: { DestinationView(isPresenting: $isItemViewPresenting, game: game.title) })
                             VStack (spacing: 8){
-                                CardView(title: i.title, description: i.description)
+                                CardView(title: game.title, description: game.description)
                             }
+                            .foregroundColor(.black)
+                            .multilineTextAlignment(.leading)
                             
+                            
+                            //                            Button {
+                            //                                selectedItem = game
+                            //                                isItemViewPresenting = true
+                            //
+                            //                            } label: {
+                            //                                VStack (spacing: 8){
+                            //                                    CardView(title: game.title, description: game.description)
+                            //                                }
+                            //                                .foregroundColor(.black)
+                            //                                .multilineTextAlignment(.leading)
+                            //                            }
+                            //
+                            //                        }.background(
+                            //                            NavigationLink(destination: DestinationView(isPresenting: $isItemViewPresenting, game: selectedItem.title)) {EmptyView()}
+                            //                        )
                         }
-                    }
-                }.padding()
+                    }.padding()
+                        .offset(x: 0, y: -40)
                     
+                    
+                    
+                }.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.65, alignment: .center)
                 
-            }.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.65, alignment: .center)
                 
-            
+            }
         }
     }
 }
@@ -64,7 +100,7 @@ struct CardView: View {
                     .fill(Color.gray)
                     .cornerRadius(8)
                     .frame(width: UIScreen.main.bounds.width * 0.52, height: UIScreen.main.bounds.height * 0.33, alignment: .center)
-                    
+                
                 
                 VStack (alignment: .leading, spacing: 4){
                     Text(title)
@@ -89,14 +125,17 @@ struct BackgroundViewSelectGameView: View {
         LinearGradient(gradient: Gradient(colors: [Color("Ocean"), Color("Aqua")]),
                        startPoint: .topLeading,
                        endPoint: .bottomTrailing)
-            .edgesIgnoringSafeArea(.all)
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
 struct SelectGameView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            SelectGameView()
+            NavigationView {
+                SelectGameView()
+            }
         }
     }
 }
+
