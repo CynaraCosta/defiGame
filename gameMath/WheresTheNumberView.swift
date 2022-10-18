d0f59ad635b3479ae8c04780bbbf245f10135117
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-10-18 08:56:42 -0300

    creating WherestheNumberView

---
+++ gameMath/WheresTheNumberView.swift
@@ -0,0 +1,20 @@
+//
+//  WheresTheNumberView.swift
+//  gameMath
+//
+//  Created by Danielly Santos Lopes da Silva on 18/10/22.
+//
+
+import SwiftUI
+
+struct WheresTheNumberView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WheresTheNumberView_Previews: PreviewProvider {
+    static var previews: some View {
+        WheresTheNumberView()
+    }
+}
