01c7bbcd4ddaecfaef161b08087c32f4283c1364
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-10-20 09:27:21 -0300

    creating NumberCorrectVuew

---
+++ gameMath/Componentes/NumberCorrectView.swift
@@ -0,0 +1,20 @@
+//
+//  NumberCorrectView.swift
+//  gameMath
+//
+//  Created by Danielly Santos Lopes da Silva on 20/10/22.
+//
+
+import SwiftUI
+
+struct NumberCorrectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NumberCorrectView_Previews: PreviewProvider {
+    static var previews: some View {
+        NumberCorrectView()
+    }
+}
