a67848eace89a89c7f17d348dfb8800d39e36487
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-10-20 10:19:28 -0300

    creatin

--- gameMath/Models/NumberCorrectModel.swift
+++ gameMath/Models/NumberCorrectModel.swift
@@ -6,3 +6,26 @@
 //
 
 import SwiftUI
+
+class NumberCorrectModel: Hashable {
+    let id: UUID
+    let syllable: String
+    let toGuess: Bool
+    
+    
+    init(syllable: String, toGuess: Bool = false) {
+        self.id = UUID()
+        self.syllable = syllable
+        self.toGuess = toGuess
+    }
+    
+    static func == (lhs:NumberCorrectModel, rhs: NumberCorrectModel) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(syllable)
+        hasher.combine(toGuess)
+    }
+}
