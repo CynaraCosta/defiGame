16b85e74ea91a98a874c64ecb168fd28750db32e
CynaraCosta <cvocac@cin.ufpe.br>
2022-10-21 11:06:08 -0300

    added the haptic to button when correct answer too

--- gameMath/Views/MaioraisView.swift
+++ gameMath/Views/MaioraisView.swift
@@ -11,7 +11,7 @@ struct MaioraisVIew: View {
     
     @State var timerRunning = false
     @State private var initPopUp = true
-    @State private var blurAmount: CGFloat = 40.0
+    @State private var blurAmount: CGFloat = 32.0
     @State private var textGame: String = "Selecionar o maior número disposta na tela."
     
 //    private var data: [Int] = Array(1...9)
@@ -29,6 +29,7 @@ struct MaioraisVIew: View {
     ]
     
     @State private var points = 0
+    @State var buttonClicked = false
     
     let timerTimer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     @State var time: Int = 40
@@ -39,7 +40,7 @@ struct MaioraisVIew: View {
             ZStack {
                 MaioraisBackgroundView()
                 
-                VStack (spacing: UIScreen.main.bounds.height * 0.06) {
+                VStack (spacing: UIScreen.main.bounds.height * 0.04) {
                     
                     HStack {
                         Image(systemName: "star.fill")
@@ -47,7 +48,7 @@ struct MaioraisVIew: View {
                         Circle()
 //                                .foregroundColor(.white)
 //                                .font(.system(size: 40, weight: .semibold))
-                            .fill(.black)
+                            .fill(.clear)
                             .frame(width: UIScreen.main.bounds.width * 0.2, height: UIScreen.main.bounds.height * 0.2, alignment: .center)
                         
                             .modifier(AnimatingNumberOverlay(number: CGFloat(points)))
@@ -55,6 +56,7 @@ struct MaioraisVIew: View {
                     
                     ProgressBar(width: UIScreen.main.bounds.width * 0.7, height: UIScreen.main.bounds.height * 0.03, percent: CGFloat(time), color: time > 20 ? .green : ((time > 5) ? .yellow : .red))
                         .animation(.spring())
+                        .offset(x: 0, y: -48)
                     
                     Text("Encontre o maior número!")
                         .font(.system(size: 22, weight: .semibold))
@@ -62,30 +64,41 @@ struct MaioraisVIew: View {
                     
                     LazyVGrid(columns: adaptiveColumns, spacing: 20) {
                         ForEach(data, id: \.self) { number in
-                            Button {
+                            Button (action: {
                                 if number == answer {
                                     withAnimation {
                                         points += 10
                                     }
                                     playSound(sound: "yeah")
-                                    data = generateNumbers()
+                                    HapticManager.instance.impact(style: .light)
                                     
                                 } else {
                                     HapticManager.instance.notification(type: .error)
-                                    data = generateNumbers()
                                     playSound(sound: "windows")
                                 }
-                            } label: {
+                                
+                                
+                                buttonClicked = true
+                                data = generateNumbers()
+                                buttonClicked = false
+                                
+                            }, label: {
                                 ZStack {
                                     Rectangle()
                                         .frame(width: UIScreen.main.bounds.width * 0.25, height: UIScreen.main.bounds.height * 0.11)
                                         .cornerRadius(8)
                                         .foregroundColor(.white)
+//                                        .background(
+//                                            RoundedRectangle(cornerRadius: 5)
+//                                                .offset(x: 0, y: 4)
+//                                                .fill(buttonClicked ? .blue : .red)
+//                                        )
+                                                        
                                     Text("\(number)")
                                         .foregroundColor(Color("Blue1000"))
-                                        .font(.system(size: 16, weight: .regular))
+                                        .font(.system(size: 18, weight: .regular))
                                 }
-                            }
+                            })
                             
                         }
                     }.padding()
@@ -124,6 +137,7 @@ func generateNumbers() -> [Int] {
     }
 //    return (1..<10).map{_ in Int.random(in: 1 ... 99)}
     // [1, 45, 67, 34, 98, 6, 78, 65, 23]
+
     return Array(set)
 }
 
