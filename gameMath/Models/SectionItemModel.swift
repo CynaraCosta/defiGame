c38c8154bb60d149d5dd42448717be300e33659a
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-10-21 11:09:51 -0300

    functions in SectionItemModel

--- gameMath/Models/SectionItemModel.swift
+++ gameMath/Models/SectionItemModel.swift
@@ -5,4 +5,47 @@
 //  Created by Danielly Santos Lopes da Silva on 21/10/22.
 //
 
-import Foundation
+import SwiftUI
+
+
+class SectionItemModel {
+    let activities: [ActivityItemModel]
+    
+    init(activities: [ActivityItemModel]){
+        self.activities = activities
+        
+    }
+    
+    
+    static func initNumber() -> SectionItemModel {
+        
+        //model de numero escrito
+        let atividadeUm: [NumberCorrectModel] = [
+            NumberCorrectModel(syllable: "DOZE", toGuess: true),
+            
+            ]
+            
+            //botoes para advinhar
+            let atividadeUmBotao: [GameButtonModel] = [
+                GameButtonModel(syllable: "12", buttonColor: .cyan, textColor: .white, isCorrect: true),
+                GameButtonModel(syllable: "12", buttonColor: .cyan, textColor: .white, isCorrect: true),
+                GameButtonModel(syllable: "12", buttonColor: .cyan, textColor: .white, isCorrect: true),
+                GameButtonModel(syllable: "12", buttonColor: .cyan, textColor: .white, isCorrect: true),
+                GameButtonModel(syllable: "12", buttonColor: .cyan, textColor: .white, isCorrect: true),
+                GameButtonModel(syllable: "12", buttonColor: .cyan, textColor: .white, isCorrect: true),
+                GameButtonModel(syllable: "12", buttonColor: .cyan, textColor: .white, isCorrect: true),
+                GameButtonModel(syllable: "12", buttonColor: .cyan, textColor: .white, isCorrect: true),
+                GameButtonModel(syllable: "12", buttonColor: .cyan, textColor: .white, isCorrect: true)
+                
+       
+            ]
+        
+        let atividadeUmBotao: ActivityItemModel = ActivityItemModel(gameButton: atividadeUmBotao, numberCorrect: atividadeUm)
+            
+            
+        
+        return SectionItemModel(activities: [atividadeUm, ])
+        
+    }
+    
+}
