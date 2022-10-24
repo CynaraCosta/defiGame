dde3ae039e6352c14d7177b2cad09cdddb22bfd9
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-10-24 10:29:04 -0300

    adding settings in wheres the number view file

--- gameMath/Views/WheresTheNumberView.swift
+++ gameMath/Views/WheresTheNumberView.swift
@@ -8,13 +8,48 @@
 import SwiftUI
 
 struct WheresTheNumberView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    let activities: [ActivityItemModel]
+    @State var activityIndex: Int = 0
+    
+    init(activities: [ActivityItemModel]) {
+        self.activities = activities
+    }
+    
+    func changeListActivityIndex() -> Void {
+        if (
+            (self.activityIndex + 1) <= (self.activities.count - 1)) {
+            self.activityIndex += 1
+        } else {
+            self.presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // sentenca para advinhar
+        
+        LazyVGrid(columns: [
+            GridItem(.adaptive(minimum: 100))
+        ],
+                  spacing: 20) {
+            ForEach(activities[activityIndex].numberCorrect, id: \.id ) {
+                numberCorrect in
+                NumberCorrectView(
+                    textButton: numberCorrect.syllable ,
+                    toGuess: numberCorrect.toGuess)
+            }
+        }
+        
+        
     }
-}
-
-struct WheresTheNumberView_Previews: PreviewProvider {
-    static var previews: some View {
-        WheresTheNumberView()
+    
+    struct WheresTheNumberView_Previews: PreviewProvider {
+        static var previews: some View {
+            WheresTheNumberView(activities: SectionItemModel.initGameSolo().activities)
+        }
     }
+    
 }
