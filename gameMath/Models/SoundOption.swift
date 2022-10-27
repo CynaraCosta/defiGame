215ae7b5b895967341822eba2df8a4d323a407c5
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-10-27 10:12:28 -0300

    creating two sound files

---
+++ gameMath/Models/SoundOption.swift
@@ -0,0 +1,13 @@
+//
+//  SoundOption.swift
+//  gameMath
+//
+//  Created by Danielly Santos Lopes da Silva on 27/10/22.
+//
+
+enum SoundOption: String {
+    case windows
+    case yeah
+    
+    
+}
